3592a62a7a332d573c2867830f2b26fc8446c1e0
Thiago Silva <thiago.tlsbomu@gmail.com>
2022-11-08 17:39:35 -0300

    Add feature user

---
+++ app/database/seeders/Admin/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders\Admin;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+            [
+                'name' => 'admin',
+                'email' => 'admin@e2.com.br',
+                'email_verified_at' => '2022-11-08',
+                'password' => bcrypt('123456')
+            ]
+        );
+    }
+}
