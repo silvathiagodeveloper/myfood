10b273dade44ba7ec672399d4686d9fa03ecc72a
Thiago Silva <thiago.tlsbomu@gmail.com>
2022-11-29 12:16:59 -0300

    Fixed interface base

--- app/app/Interfaces/BaseRepositoryInterface.php
+++ app/app/Interfaces/BaseRepositoryInterface.php
@@ -4,6 +4,7 @@ namespace App\Interfaces;
 
 interface BaseRepositoryInterface
 {
+    public function first(array $order = null, array $with = null);
     public function getAll(array $order = null, array $with = null);
     public function getAllPaginate(int $qtty = 15);
     public function getById(int $id);
