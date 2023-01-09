e19f1e960fea12b07e4489bade3306cdb5582378
Thiago Silva <thiago.tlsbomu@gmail.com>
2023-01-09 14:07:18 -0300

    Added products feature

--- front/vuefood/src/pages/Products.vue
+++ front/vuefood/src/pages/Products.vue
@@ -16,127 +16,26 @@
         </div>
         <!-- Cards Produtos -->
         <div class="row my-4">
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/pizza.png" alt="">
+            <div class="col-lg-4 col-md-6 mb-4" v-for="(product, index) in products.data" :key="index">
+                <div class="card--flat h-100">
+                    <a href="#">
+                        <div class="card-image">
+                            <img v-if="image" class="card-img-top" :src="image" alt=""/>
+                            <img v-else class="card-img-top" src="@/assets/imgs/pizza.png" alt=""/>
+                        </div>
+                    </a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">{{ product.name }}</a>
+                        </h4>
+                        <h5>R$ {{ product.price }}</h5>
+                        <p class="card-text">{{ product.description}}</p>
+                    </div>
+                    <div class="card-footer card-footer-custom">
+                        <router-link :to="{name: 'cart'}">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></router-link>
+                    </div>
                 </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Pizza</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
             </div>
-            <div class="card-footer card-footer-custom">
-                <router-link :to="{name: 'cart'}">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></router-link>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/acai.png" alt="">
-                </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Açai na Tijela</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-            <div class="card-footer card-footer-custom">
-                <a href="carrinho.html">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="disabled card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/lanches.png" alt="">
-                </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Sanduiche</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-            <div class="card-footer card-footer-custom">
-                <a href="carrinho.html">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/pizza.png" alt="">
-                </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Pizza</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-            <div class="card-footer card-footer-custom">
-                <a href="carrinho.html">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/japonesa.png" alt="">
-                </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Japonesa</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-            <div class="card-footer card-footer-custom">
-                <a href="carrinho.html">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
-            </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card--flat h-100">
-            <a href="#">
-                <div class="card-image">
-                <img class="card-img-top" src="imgs/acai.png" alt="">
-                </div>
-            </a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">Açai na Tijela</a>
-                </h4>
-                <h5>R$ 12,99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-            <div class="card-footer card-footer-custom">
-                <a href="carrinho.html">Adicionar no Carrinho <i class="fas fa-cart-plus"></i></a>
-            </div>
-            </div>
-        </div>
-
         </div>
         <!-- /.row -->
     </div>
@@ -155,6 +54,8 @@ export default {
         }
         this.getCategories(this.tenant.id)
             .catch(response => { toast.error('Falha ao carregar categorias!') })
+        this.getProducts(this.tenant.id)
+            .catch(response => { toast.error('Falha ao carregar produtos!') })
     },
 
     computed: {
